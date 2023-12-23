71825d607da093b0910e234dc3878abd30c0d3ef
kaung-min-khant <1kaungminkhant1@gmail.com>
2023-12-23 12:38:05 -0800

    Initial Commit

---
+++ Koe Na Win Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Koe Na Win Tracker
+//
+//  Created by Kaung Min Khant on 12/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Koe Na Win Tracker/Koe_Na_Win_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Koe_Na_Win_TrackerApp.swift
+//  Koe Na Win Tracker
+//
+//  Created by Kaung Min Khant on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Koe_Na_Win_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
